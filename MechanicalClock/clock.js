ae75b354c79760f6cfbae2c839226bdf7c4c2d08
Han, Sanghoon <wildone@postech.ac.kr>
2013-10-31 16:53:36 +0900

    Basic Clock structure

--- MechanicalClock/clock.js
+++ MechanicalClock/clock.js
@@ -1,7 +1,62 @@
-function MechanicalClock() {
-	
-}
+(function($){
+	var units = [{name:"vps",op:"divide",by:1},
+			 {name:"bps",op:"divide",by:1},
+			 {name:"vpm",op:"divide",by:60},
+			 {name:"bpm",op:"divide",by:60},
+			 {name:"vph",op:"divide",by:60*60},
+			 {name:"bph",op:"divide",by:60*60},
+			 {name:"hz",op:"multiply",by:2}];
+	$.each(units, function(idx, unit) {
+		if(unit.op === "divide") {
+			unit.multiplyFactor = 1/unit.by;
+		} else if(unit.op === "multiply") {
+			unit.multiplyFactor = unit.by;
+		}
+	});
+	function MechanicalClock(interval) {
+		//default : second hand tick occurs 1 time per sec.
+		//If 3Hz, or 3*2*3600=21600vPh(vibration per hour) is to be simulated,
+		//tickInterval value should be calcuated to a value 1000/(3*2), which means
+		//6 jumps per second.
+		//for 4Hz, or 4*2*3600=28,800vPh, 1000/(4*2) = 125ms. 8 jumps per second.
+		//for 2.5Hz, or 2.5*2*3600=18,800vPh, 1000/(2.5*2) = 200ms. 5 jumps per second.
+		//480vpm or bpm -> 480/60=8 jumps per second
+		this.jumpsPerSecond = 1;
+		this.interval = "1vps";
+		if(interval !== undefined && interval !== null) {
+			this.setInterval(interval);
+		}
+		this.hands = {
+			hour : 0,
+			minute : 0,
+			second : 0
+		};
+	}
+	MechanicalClock.prototype.setInterval = function(interval) {
+		var self = this;
+		if(interval === undefined || interval === null) {
+			interval = this.interval;
+		}
+		if(typeof interval === "number") {
+			interval = (interval + "vps");
+		}
+		$.each(units, function(idx,unit) {
+			if(interval.toLowerCase().indexOf(unit.name.toLowerCase())>=0) {
+				var value = Number(interval.toLowerCase().split(unit.name.toLowerCase())[0])
+					* unit.multiplyFactor;
+				if(!isNaN(value)) {
+					self.jumpsPerSecond = value|0;//integer only.
+					self.interval = interval;
+					return false;
+				}
+			}
+		});
+	};
+	MechanicalClock.prototype.startBeat = function() {
 
-MechanicalClock.prototype.init = function(option) {
+	};
+	MechanicalClock.prototype.stopBeat = function() {
 
-};
+	};
+	window.MechanicalClock = MechanicalClock;
+})(Zepto);
