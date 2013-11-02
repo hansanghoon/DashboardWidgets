6bbc88e1f7b4e043780c4838678b069eaccf1179
Han, Sanghoon <wildone@postech.ac.kr>
2013-11-03 00:02:56 +0900

    Basic SVG Design, Dashboard widget

--- MechanicalClock/widget.js
+++ MechanicalClock/widget.js
@@ -1,19 +1,74 @@
 $(function() {
-	//Sample
-	var ticker = new TickGenerator();
+	var interval = null;
+	if(window.widget) {
+		interval = widget.preferenceForKey('interval');
+	}
+	var ticker = new TickGenerator(interval);
+	window.ticker = ticker;
+	var hourhand = $('#hourhand');
+	var minhand = $('#minutehand');
+	var sechand = $('#secondhand');
 	ticker.addTickHandler(function() {
 		var args = $.makeArray(arguments).slice(0,TickGenerator.I_MILLI+1);
-		$('#time').text(args.join(' , '));
-	});
-	ticker.start();
-	$('#active').prop('checked',true).change(function() {
-		if(this.checked) {
-			ticker.start();
-		} else {
-			ticker.stop();
-		}
+        var s = args[TickGenerator.I_SECOND] * 1000 + args[TickGenerator.I_MILLI];
+		var m = args[TickGenerator.I_MINUTE] * 60 * 1000 + s;
+        var h = (args[TickGenerator.I_HOUR]%12) * 3600 * 1000 + m
+        hourhand.attr('transform', 'rotate('+(h*30/(3600*1000))+',50,50)');
+        minhand.attr('transform',  'rotate('+(m*6/(60*1000))   +',50,50)');
+        sechand.attr('transform',  'rotate('+(s*6/(1000))      +',50,50)');
 	});
 	$('#rate').change(function() {
-		ticker.setInterval($(this).find('option:selected').val());
+		var value = $(this).find('option:selected').val();
+		if(window.widget) {
+			widget.setPreferenceForKey('interval', value);
+		}
+		ticker.setInterval(value);
 	});
+	function changeInterval(value) {
+		if(!value) return;
+		$('#rate').find('[value="'+interval+'"]').prop('selected',true);
+		ticker.setInterval(value);
+	}
+	ticker.start();
+	function showPrefs() {
+		if(window.widget) {
+			widget.prepareForTransition('ToBack');
+			var intv = widget.preferenceForKey('interval');
+			changeInterval(intv);
+		}
+		$('.widget.front').removeClass('active');
+		$('.widget.back').addClass('active');
+		if (window.widget) {
+	        setTimeout ('widget.performTransition();', 0);
+	    }
+	}
+	function hidePrefs() {
+		if(window.widget) {
+			widget.prepareForTransition('ToFront');
+		}
+		$('.widget.front').addClass('active');
+		$('.widget.back').removeClass('active');
+		if (window.widget) {
+	        setTimeout ('widget.performTransition();', 0);
+	    }
+	}
+	$('#infobutton').hover(function(){$(this).animate({'opacity':1});}, 
+						   function(){$(this).animate({'opacity':0});} )
+		.css('opacity',0)
+		.click(function() {
+			if(!$(this).is(':visible')) {
+				return;
+			}
+			showPrefs();
+		});
+	$('#backbutton').click(hidePrefs);
+	if(window.widget) {
+		window.widget.onshow = function() {
+			ticker.tick();
+			ticker.start();
+		};
+		window.widget.onhide = function() {
+			ticker.stop();
+		};
+	}
 });
